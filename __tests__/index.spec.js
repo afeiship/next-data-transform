162c62584c19efeed33dce79ca15c43bcce45348
afeiship <1290657123@qq.com>
2021-05-21 12:50:59 +0800

    feat: test case/ignore dist

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -18,5 +18,17 @@
       const res = NxDataTransform.raw('key=1&value=2');
       expect(res).toBe('key=1&value=2');
     });
+
+    test('json with string value', () => {
+      var data = '{"key":1,"value":2}';
+      const res = NxDataTransform.json(data);
+      expect(res).toBe('{"key":1,"value":2}');
+    });
+
+    test('urlencoded with null value', () => {
+      var data = null;
+      const res = NxDataTransform.urlencoded(data);
+      expect(res).toBe(null);
+    });
   });
 })();
